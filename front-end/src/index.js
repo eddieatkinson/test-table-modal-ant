b4148205a0d03b71f47b1fc7c460d4f4b4a64d82
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-04-18 18:08:06 -0400

    redux is hooked up to the front-end

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { createStore, applyMiddleware } from "redux";
+
+import RootReducer from "./redux/reducers/RootReducer";
+import reduxPromise from "redux-promise";
+import { Provider } from "react-redux";
+
+const theStore = applyMiddleware(reduxPromise)(createStore)(RootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={theStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ front-end/src/redux/reducers/DataReducer.js
@@ -0,0 +1,83 @@
+import { GET_VENDORS_ACTION, GET_INVOICES_ACTION } from "../types";
+
+const INITIAL_STATE = {
+  //sample to show redux is hooked up:
+  vendors: [
+    {
+      vendorId: "D1",
+      vendorName: "Delmonte",
+      creditBal: 600.0,
+    },
+    {
+      vendorId: "T1",
+      vednorName: "Target",
+    },
+    {
+      vendorId: "W1",
+      vendorName: "Walmart",
+      creditBal: 12.25,
+    },
+    {
+      vendorId: "G1",
+      creditBal: 0.0,
+    },
+  ],
+  invoices: [
+    {
+      invoiceId: 1234,
+      vendorId: "G1",
+      quantity: 20,
+      product: "Apple",
+      amountBal: 129.92,
+      amountDue: 25.5,
+      invoiceDate: "04 / 01 / 2020",
+    },
+    {
+      invoiceId: 4578,
+      vendorId: "Delmonte",
+      product: "B1",
+      quantity: 500,
+      amountBal: 1024.12,
+      amountDue: 512.5,
+      invoiceDate: "03 / 31 / 2020",
+    },
+    {
+      invoiceId: 9999,
+      vendorId: "W1",
+      quantity: 1000,
+      Product: "Napkin",
+      amountBal: 12.25,
+      amountDue: 12.25,
+      invoiceDate: "03 / 31 / 2020",
+    },
+    {
+      invoiceId: 1000,
+      vendorId: "W1",
+      quantity: 25,
+      Product: "Sanitizer",
+      amountBal: 25.0,
+      amountDue: 12.25,
+      invoiceDate: "03 / 31 / 2020",
+    },
+    {
+      invoiceId: 1025,
+      vendorId: "W1",
+      quantity: 1000,
+      Product: "Napkin",
+      amountBal: 0,
+      amountDue: 0,
+      invoiceDate: "03 / 31 / 2020",
+    },
+  ],
+};
+
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case GET_INVOICES_ACTION:
+      return { ...state, invoices: action.payload };
+    case GET_VENDORS_ACTION:
+      return { ...state, vendors: action.payload };
+    default:
+      return state;
+  }
+}
