5ed902cff12c6ed2f4317bfd86c595d1a8f62f67
Eddie Atkinson <eddiebatkinson@gmail.com>
2020-04-19 11:16:17 -0400

    WIP added lodash and inserted table with headers from config

--- front-end/src/containers/Table.js
+++ front-end/src/containers/Table.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { DatePicker } from "antd";
+import { Table as AntTable } from "antd";
+import { forEach } from "lodash";
 import "antd/dist/antd.css";
 
 class Table extends Component {
+  getColumns() {
+    const columns = [];
+    this.props.tableConfig &&
+      forEach(this.props.tableConfig.columns, (column) => {
+        if (column.display) {
+          columns.push({
+            title: column.displayName,
+            dataIndex: column.fieldName,
+          });
+        }
+      });
+    if (this.props.tableConfig && this.props.tableConfig.paymentEnabled) {
+      columns.push({
+        title: "Pay",
+        dataIndex: "pay",
+      });
+    }
+    return columns;
+  }
   render() {
-    return <DatePicker />; // To make sure it works
+    const columns = this.getColumns();
+    return <AntTable columns={columns} />; // To make sure it works
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     data: state.data,
-    config: state.config,
+    tableConfig: state.config.configData.tableConfig,
   };
 };
 
